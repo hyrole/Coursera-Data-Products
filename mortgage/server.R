abd97939a19f051e4981d58aa16c889108a013fa
hyrole <hyrole@gmail.com>
2016-02-29 09:14:43 +0800

    update shiny

--- mortgage/server.R
+++ mortgage/server.R
@@ -1,3 +1,7 @@
+#File name: server.R
+#Author: Hyrole SoASWT
+#Title: Mortgage Calculator
+
 # Mortgage Functions - formula & calculation
 CalculateTotalInterest<-function(getAmount, getInterest, getYears) {
   if (getAmount==0 | getAmount==0 | getYears==0) return(NA) 
@@ -14,52 +18,28 @@ CalculateMonthlyPayment<-function(getTotalInterest, getYears) {
   else return(round(getTotalInterest/getYears/12,2))
 }
 
-CalculateEndDate<-function(getStartDate, getYears) {
-  if (getStartDate==0 | getYears==0) return(NA) 
-  else {
-    #convertDate <- as.Date(getStartDate, format = "%m/%d/%Y")
-    #return(getStartDate+(getYears*365))
-    return(getStartDate)
-  }
-}
 
 library(shiny)
-
-# Sample Apps:
-# Mortgage> http://www.bankrate.com/calculators/mortgages/mortgage-calculator.aspx
-# Mortgage> https://www.zillow.com/mortgage-calculator/
-# Fuel> http://journeyprice.co.uk/
-# Fuel> http://www.fuel-economy.co.uk/calc.html
-
-
-# Available datasets
-# https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/00Index.html
-# Historical Exchange Rates: http://www.oanda.com/currency/historical-rates/
-
-# Run option: runApp("myshinyapp", display.mode = "showcase")
-
+library(ggplot2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
   # Load dataset
-  dataset <- read.csv("./data/currency.csv")
+  dataset <- read.csv("./data/mortgageus10yrs.csv")
   
   # Show the first observations
   output$dataSurvey <- renderTable({
     head(dataset, 10)
   })  
   
-  # Get input from UI
+  # Get inputs from UI screen
   amount <- reactive({as.numeric(input$mgAmount)})
   interest <- reactive({as.numeric(input$mgInterest)})
   years <- reactive({as.numeric(input$mgYears)})
-  startdate <- renderPrint({input$mgStartDate})  
-  
-  
-  # Show result on button pressed
-  
-  # 
+  enddate <- renderPrint({(input$mgStartDate)+(years()*365)})
+    
+  # Show output/results
   output$TotalRepayment <- renderText({
     input$btnCalculate
     isolate(CalculateTotalRepayment(CalculateTotalInterest(amount(), interest(), years()), amount()))
@@ -67,7 +47,7 @@ shinyServer(function(input, output) {
   
   output$TotalPrincipal <- renderText({
     input$btnCalculate
-    #isolate(CalculateMonthlyPayment(CalculateInterest(amount(), interest(), years()), years()))
+    isolate(amount())
   })  
   
   output$TotalInterest <- renderText({
@@ -81,27 +61,43 @@ shinyServer(function(input, output) {
   }) 
   
   output$EndDate <- renderText({
-    input$btnCalculate
-    isolate(CalculateEndDate(startdate(), years()))
+    input$btnCalculate    
+    isolate(enddate())
   })    
   
-  # You can access the values of the widget (as a vector of Dates)
-  # with input$dates, e.g.
-  output$value <- renderPrint({ input$date })
-  
-  
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  
-  output$distPlot <- renderPlot({
-    x    <- faithful[, 2]  # Old Faithful Geyser data
-    years <- seq(min(x), max(x), length.out = input$years + 1)
-    
-    # draw the histogram with the specified number of years
-    hist(x, breaks = years, col = 'skyblue', border = 'white')
+  # Generate bar plot
+  output$plotSummary <- renderPlot({
+    datam <- c(CalculateTotalRepayment(CalculateTotalInterest(amount(), interest(), years()), amount()),
+           amount(), 
+           CalculateTotalInterest(amount(), interest(), years()))
+    color <- c('skyblue', 'red1', 'green')
+    bp <- barplot(datam, col = color, border = 'white', main="Mortgage Distribution", horiz=TRUE,
+            names.arg=c("Repayment", "Principal", "Interest"))  
+    ## Add text at top of bars
+    text(y = bp, x = datam, label = datam, pos = 2, cex = 1.2, col = "white")    
   })
+  
+  rates <- aggregate(. ~ YEAR, dataset[-1], mean)
+  
+  # Show the dataset observations
+  output$dataMortgageRate <- renderTable({
+    head(rates, 10)
+  })  
+  
+  # Generate line plot
+  output$plotMortgageRate <- renderPlot({
+    ggplot(rates, aes(x=YEAR, y=VALUE, colour="red")) + geom_line(stat="identity", color = "red", lwd = 1) + 
+      labs(x="Years", y="Interes Rates") 
+  })  
 })
+
+# Sample Apps:
+# Mortgage> http://www.bankrate.com/calculators/mortgages/mortgage-calculator.aspx
+# Mortgage> https://www.zillow.com/mortgage-calculator/
+# Fuel> http://journeyprice.co.uk/
+# Fuel> http://www.fuel-economy.co.uk/calc.html
+
+# Available datasets
+# https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/00Index.html
+# Historical Exchange Rates: http://www.oanda.com/currency/historical-rates/
+# Run option: runApp("myshinyapp", display.mode = "showcase")
--- mortgage/ui.R
+++ mortgage/ui.R
@@ -1,53 +1,77 @@
+#File name: ui.R
+#Author: Hyrole SoASWT
+#Title: Mortgage Calculator
+
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 shinyUI(pageWithSidebar(
   headerPanel("Mortgage Calculator"),
-  sidebarPanel(
-    numericInput(inputId="mgAmount", label="Mortgage Amount ($)", value= 300000,min=0),
-    numericInput(inputId="mgYears", label="Mortgage term in years", value= 15,min=0),
-    numericInput(inputId="mgInterest", label="Interest rate per year (%)", value= 5,min=0),
+  sidebarPanel(  
+    numericInput(inputId="mgAmount", label="Mortgage Amount ($)", value= 450000,min=0),    
+    sliderInput("mgYears",
+                "Mortgage term in years",
+                min = 5,
+                max = 35,
+                value = 15),      
+    numericInput(inputId="mgInterest", label="Interest rate per year (%)", value= 5,min=1, max=10),
     dateInput("mgStartDate", label = "Mortgage start date", value = "2016-03-01"),    
-    actionButton("btnCalculate", "Calculate"),
-    br(),
-    p(strong(em("Documentation:",a("Mortgage Calculator: How to calculate",href="READMe.html")))),
-    p(strong(em("Github repository:",a("Developing Data Products - Peer Assessment Project; Shiny App",href="https://github.com/CrazyFarang/DevelopingDataProducts"))))    
+    actionButton("btnCalculate", "Calculate"),      
+    br(),br(),
+    p(strong(em("Reference:",a("Mortgage Calculator: How to calculate",href="http://www.bankrate.com/")))),
+    p(strong(em("Github repository:",a("Developing Data Products - Peer Assessment Project; Shiny App",href="https://github.com/hyrole/Coursera-Data-Products"))))    
   ),
   mainPanel(
     tabsetPanel(
       tabPanel('Your results',
-               h5('Total repayment'),
-               verbatimTextOutput("TotalRepayment"),                
-               h5('Total interest'),
+               h5('Total repayment ($)'),
+               verbatimTextOutput("TotalRepayment"),  
+               h5('Total principal ($)'),
+               verbatimTextOutput("TotalPrincipal"),                
+               h5('Total interest ($)'),
                verbatimTextOutput("TotalInterest"),     
-               h5('Monthly payments'),
+               h5('Monthly payments ($)'),
                verbatimTextOutput("MonthlyPayment"),    
                h5('Estimated payment End Date'),
-               verbatimTextOutput("EndDate"),                 
-               p("Source: ", a("How to calculate mortgage", 
-                               href = "http://www.bankrate.com/"))
+               verbatimTextOutput("EndDate"),                                  
+               p("Reference: ", a("How to calculate mortgage", 
+                               href = "http://www.bankrate.com/")),
+               p("Mortgage is a loan to finance the purchase of real property. 
+                 It is used by purchasers of to raise funds to buy real estate like a house, 
+                 apartment, or shoplot; or by existing property owners to raise funds for any 
+                 purpose while putting a lien on the property being mortgage"),               
+               p("A mortgage is actually made up of several parts - the collateral you used 
+                  to secure the loan, your principal and interest payments, taxes and insurance.
+                  In this assingment, I will demonstrate a simple calculator to calculate 
+                  total repayment, total interest and monthly payments based on the loan amount
+                  and estimated current interest rate as well as mortgage term in years. 
+                  This information will be used to estimate your monthly payments and see the 
+                  effect of adding extra payments."),               
+               p(em("Fill up information on the left side to see the results. Don't forget to click
+                    on 'Calculate' button to perform changes."))
       ),
-      tabPanel('Data Summary',
-               h5('Available data for'),
-               h5('Recent BMI Indicators'),
-               p("Source: ", a("KNOEMA-WHO Global Database on Body Mass Index (BMI)", 
-                               href = "http://knoema.com/WHOGDOBMIMay/who-global-database-on-body-mass-index-bmi"))
+      tabPanel('Plot: Summary',
+               plotOutput("plotSummary"),               
+               br(),
+               p("Above barplots will illustrates total loan repayment, total principal and total 
+                  interest based on estimated interest rate per year and total term in years. 
+                 Slide the 'Mortgage term in years' to view the responsive results.")
       ),
-      tabPanel('Plot: Interest Rates',
-               h5('Recent BMI Indicators'),
-               p("Plotted data are for your gender for your specific country"),
-               p("If there are no data for your gender, plotted data are for adults for your specific country"),
-               p("Worldwide Data: ", a("KNOEMA-WHO Global Database on Body Mass Index (BMI)", href = "http://knoema.com/WHOGDOBMIMay/who-global-database-on-body-mass-index-bmi"))
-      ),
-      tabPanel('Plot: Payments Schedule',
-               h5('Recent BMI Indicators'),
-               p("Plotted data are for your gender for your specific country"),
-               p("If there are no data for your gender, plotted data are for adults for your specific country"),
-               p("Worldwide Data: ", a("KNOEMA-WHO Global Database on Body Mass Index (BMI)", href = "http://knoema.com/WHOGDOBMIMay/who-global-database-on-body-mass-index-bmi"))
+      tabPanel('Plot: Mortgage Rates', 
+               br(),
+               p(strong("10-Year Fixed Rate Mortgage Average in the United States")), 
+               br(),
+               column(1, offset = 0,
+                  br(),
+                  tableOutput("dataMortgageRate")
+               ),
+               column(10, offset =1,
+                  plotOutput("plotMortgageRate")       
+               ),
+               p("Original datasource:", a("Economic Research, Federal Reserve Bank of St. Louis", 
+                                           href = "https://research.stlouisfed.org/fred2/series/MORTGAGE30US/downloaddata"))
       )
     )
-    #,
-    #p(strong("All you need is love. But a little chocolate now and then doesn't hurt. Charles M. Schulz"))
   )
 )
 )
